6849b7b60c2c5ffd4462a8737f3690e862ca2bdd
IkpefuaTosin <tosaino34@outlook.com>
2023-07-25 17:27:50 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+
+//class Calculator
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("Enter the first number:");
+//        double num1 = double.Parse(Console.ReadLine());
+
+//        Console.WriteLine("Enter the second number:");
+//        double num2 = double.Parse(Console.ReadLine());
+
+//        Console.WriteLine("Select an operation:");
+//        Console.WriteLine("1. Addition");
+//        Console.WriteLine("2. Subtraction");
+//        Console.WriteLine("3. Multiplication");
+//        Console.WriteLine("4. Division");
+
+//        int choice = int.Parse(Console.ReadLine());
+
+//        switch (choice)
+//        {
+//            case 1:
+//                Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+//                break;
+//            case 2:
+//                Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+//                break;
+//            case 3:
+//                Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+//                break;
+//            case 4:
+//                Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+//                break;
+//            default:
+//                Console.WriteLine("Invalid choice.");
+//                break;
+//        }
+//    }
+//}
+
+//class CalculatorNum
+//{
+//    static void Main(string[] args)
+//    {
+//        // Get numbers from user;
+//        Console.Write("Enter a first number ");
+//        double num1 = double.Parse(Console.ReadLine());
+
+//        Console.Write("Enter a second number ");
+//        double num2 = double.Parse(Console.ReadLine());
+
+// Select the opperators;
+//        Console.WriteLine("1. Addition ");
+//        Console.WriteLine("2. Subtraction ");
+//        Console.WriteLine("3. Division ");
+//        Console.WriteLine("4. Multiplication ");
+//        Console.WriteLine("5. Module ");
+
+//        int selected = int.Parse(Console.ReadLine());
+
+//        switch (selected)
+//        {
+//            case 1:
+//                Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+//                break;
+//            case 2:
+//                Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+//                 break;
+//            case 3:
+//                Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+//                break;
+//            case 4:
+//                Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+//                break;
+//            case 5:
+//                Console.WriteLine($"{num1} % {num2} = {num1 % num2}");
+//                break;
+//            default:
+//                Console.WriteLine("Please select a valid intiger");
+//                break;
+
+//        }
+
+//    }
+//}
+
+//Get the user input
+//Console.Write("Enter a number ");
+//double num_1 = double.Parse(Console.ReadLine());
+
+//Console.Write("Enter a second number ");
+//double num_2 = double.Parse(Console.ReadLine());
+
+//Select an operator to be executed by;
+//Console.WriteLine("1. Addition");
+//Console.WriteLine("2. Subtraction");
+//Console.WriteLine("3. Muliplication");
+//Console.WriteLine("4. Division");
+//Console.WriteLine("5. Module");
+
+//int choice = int.Parse(Console.ReadLine());
+//switch (choice)
+//{
+//    case 1:
+//        Console.WriteLine($" {num_1} + {num_2} = {num_1 + num_2}");
+//        break;
+//    case 2:
+//        Console.WriteLine($"{num_1} - {num_2} = {num_1 - num_2}");
+//        break;
+//    case 3: Console.WriteLine($"{num_1} * {num_2} = {num_1 * num_2}");
+//        break;
+//    case 4: Console.WriteLine($"{num_1} / {num_2} = {num_1 / num_2}");
+//        break;
+//    case 5: Console.WriteLine($"{num_1} % {num_2} = {num_1 % num_2}");
+//        break;
+//    default:
+//        Console.WriteLine("Please input a valid integer");
+//        break;
+//}
